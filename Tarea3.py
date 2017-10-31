287c62cdb2c72090c8a3b332df7ad66e07009902
Nicholas <N9199@users.noreply.github.com>
2017-10-30 23:58:03 -0300

    Start

---
+++ Tarea3.py
@@ -0,0 +1,25 @@
+#To-do:
+#Fill classes
+#Design GUI
+#Program interactions
+#Do optimized backtracking (maybe DP)
+
+class Simulacion:
+    def __init__(self, parameter_list):
+        pass
+
+class Personaje:
+    def __init__(self, parameter_list):
+        pass
+
+class Prueba:
+    def __init__(self, parameter_list):
+        pass
+
+class Consumible:
+    def __init__(self, parameter_list):
+        pass
+
+class Equipamiento:
+    def __init__(self, parameter_list):
+        pass
