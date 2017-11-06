478ef54b37b6a3b33b79feaa78062f641cc2aa11
Nicholas <N9199@users.noreply.github.com>
2017-11-06 15:39:38 -0300

    Update

--- Tarea3.py
+++ Tarea3.py
@@ -1,9 +1,10 @@
 #To-do:
-#Fill classes (One is mostly ready)
+#Fill classes (Mostly ready)
 #Design GUI (Kinda ready)
 #Program interactions (Some are working)
 #Do optimized backtracking (maybe DP)(Haven't even started)
 import tkinter as tk
+import math
 from tkinter import simpledialog
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
@@ -21,14 +22,17 @@ class Simulacion(tk.Frame):
         temp[4] = "Inteligencia"
 
         for a, b in temp.items():
+            c = b.lower()
             self.stats[a] = b
             self.stats[b] = a
+            self.stats[c] = a
         self.build_GUI()
         self.start()
 
     def start(self):
+        self.count = 3
         self.load()
-        self.character = Personaje(self, self.puntos, self.base, self.tiempo)
+        self.character = Personaje(self, self.puntos, self.base, self.tiempo, quick=True)
         for i in range(6):
             temp = self.info[i].get()
             temp = temp.split(" ")
@@ -40,10 +44,10 @@ class Simulacion(tk.Frame):
         file = askopenfilename()
         if file == "":
             return
-        if len(file)<5:
+        while len(file)<5:
             messagebox.showerror("Error", "Archivo seleccionado no válido.")
             file = askopenfilename()
-        if file[-4:]!=".txt":
+        while file[-4:]!=".txt":
             messagebox.showerror("Error", "Archivo seleccionado no válido.")
             file = askopenfilename()
         
@@ -77,12 +81,10 @@ class Simulacion(tk.Frame):
     def build_GUI(self):
         self.info = []
         buttons = tk.Frame(self, pady=5, padx=10)
-        self.con = tk.Button(buttons, text="Consumir", state = tk.DISABLED, command=self.consume)
-        self.equip = tk.Button(buttons, text="Equipar", command = self._equip)
+        self.turn = tk.Button(buttons, text="Equipar", command = self._equip)
         self.Test = tk.Button(buttons, text="Dar Interrogación", state = tk.DISABLED, command=self.test)
         self.Test.pack(side="right")
-        self.equip.pack(side="right")
-        self.con.pack(side="right")
+        self.turn.pack(side="right")
         buttons.pack(side="bottom")
         frame = tk.Frame(self, padx=20)
         stats = tk.LabelFrame(master=self, text="Stats", pady=5, padx=20)
@@ -101,56 +103,112 @@ class Simulacion(tk.Frame):
         stats.pack(side="left")
         yScroll = tk.Scrollbar(frame, orient=tk.VERTICAL)
         yScroll.pack(side="right")
-        self.cons = tk.Listbox(frame, selectmode=tk.SINGLE, height=11, yscrollcommand=yScroll, width = 40)
+        self.cons = tk.Listbox(frame, selectmode=tk.SINGLE, height=11, yscrollcommand=yScroll, width = 50)
         self.cons.pack(side="left")
         frame.pack(side="right")
 
     def test(self):
         test = self.tests.pop(0)
+        messagebox.showinfo(message=str(self.tests[0]))
         self.character.taketest(test)
+        self.updatestats()
         if self.character.atributos[1]<=0:
             messagebox.showinfo(message="Perdiste")
             self.quit()
-        elif len(self.tests)>1:
-            if self.character.delta:
-                messagebox.showinfo("Resumen de la Interrogación","Lo lograste, pero perdiste "+str(self.character.delta)+" puntos de vida")
+        else:
+            if (-self.character.delta):
+                messagebox.showinfo("Resumen de la Interrogación","Lo lograste, pero perdiste "+str(-self.character.delta)+" puntos de vida")
             else:
                 messagebox.showinfo("Resumen de la Interrogación","Lo lograste, te sacaste un 7!")
-            
+        if len(self.tests)==0:
+            messagebox.showinfo("Felicitaciones", "Has ganado!")
+        self.turn.config(state = tk.ACTIVE)
+        self.count = 3
 
     def consume(self):
-        pass
+        temp = self.cons.curselection()
+        temp = temp[0]
+        asdf = self.cons.get(temp)
+        if asdf[0]=="1":
+            self.cons.delete(temp)
+        else:
+            self.cons.delete(temp)
+            self.cons.insert(temp, str(int(asdf[0])-1)+asdf[1:])
+        for i in range(len(self.consumables)):
+            if str(self.consumables[i])==asdf[2:]:
+                self.character.consume(self.consumables[i])
+                del self.consumables[i]
+                break
+        self.count -= 1
+        if self.count <= 0:
+            self.turn.config(state = tk.DISABLED)
+        self.updatestats()
 
     def _equip(self):
-        pass
+        temp = self.cons.curselection()
+        temp = temp[0]
+        self.character.equip(self.equipment[temp])
+        self.cons.delete(temp)
+        del self.equipment[temp]
+        if len(self.character.equipment)>=3:
+            self.Test.config(state = tk.ACTIVE)
+            self.turn.config(command=self.consume, text="Consumir")
+            self.cons.delete(0,tk.END)
+            temp = []
+            for item in self.consumables:
+                b = True
+                if len(temp)!=0:
+                    for a in temp:
+                        if str(item)==str(a[0]):
+                            a.append(item)
+                            b = False
+                            break
+                if b:
+                    temp.append([item])
+            
+            asdf = ""
+            for item in temp:
+                self.cons.insert(tk.END, (str(len(item))+" "+str(item[0])))
+        self.updatestats()
 
     def updatestats(self):
         for i in range(6):
             temp = self.info[i].get()
             temp = temp.split(" ")
-            self.info[i].set(temp[0]+" "+str(self.character.atributos[i]))
+            if i>1:
+                self.info[i].set(temp[0]+" "+str(math.floor(self.character.atributos[i]*self.character.bon[i-2]))+" Bonus: "+str(self.character.temp[i-2]))
+            else:
+                self.info[i].set(temp[0]+" "+str(self.character.atributos[i]))
 
 class Personaje:
 
     def __init__(self, master, puntos, vida, tiempo, quick=False):
         self.vida = int(vida)
         self.tiempo = int(tiempo)
+        self.tiempo2 = self.tiempo
         self.puntos = int(puntos)
+        self.temp = 4*[0]
+        self.bon = 4*[1]
+        self.equipment = []
+        self.consumables = []
         self.master = master
+        self.stats = self.master.stats
         self.delta = 0
         if quick:
-            pass
+            self.atributos = ["a"]+5*[0]
+            temp = self.puntos
+            for i in range(1,5):
+                self.atributos[i]+=math.floor(self.puntos/5)
+                temp -= math.floor(self.puntos/5)
+            self.atributos[5]+= temp
         else:
             self.new()
 
     def new(self):
         self.atributos = 6*[None]
-        self.temp = 4*[0]
-        self.bon = 4*[1]
-        self.equipment = []
-        self.consumables = []
         self.ask = tk.Toplevel(self.master)
-        self.stats = self.master.stats
+        self.pointsleft = tk.StringVar(value=str(self.puntos))
+        
         #for a,b in self.stats.items():
             #print(a,b)
         self.atributos[0] = simpledialog.askstring("Nombre", "Por favor ingrese el nombre del personaje")
@@ -158,13 +216,16 @@ class Personaje:
             self.atributos[0] = simpledialog.askstring("Nombre", "Por favor ingrese el nombre del personaje")
 
         messagebox.showinfo("Información","Distribuye los puntos en los stats iniciales (Tienes: "+str(self.puntos)+" puntos,"+str(self.vida)+" vida base)")
+        asdfhjk = tk.Label(self.ask, text="0", textvariable = self.pointsleft)
+        asdfhjk.pack(side="top")
+        self.pointsleft.set(value="Puntos restantes: "+str(self.puntos))
         self.values = []
         scales = []
         group_master = tk.Frame(self.ask)
         for i in range(5):
             asdf = tk.Frame(group_master, padx=20)
             b = tk.IntVar()
-            a = tk.Scale(asdf, to = 0, from_ = self.puntos, variable = b)
+            a = tk.Scale(asdf, to = 0, from_ = self.puntos, variable = b, command = self.updatelabel)
             m = tk.Label(asdf, text=self.stats[i])
             a.pack(padx=10, side="bottom")
             m.pack(pady=10, side="bottom")
@@ -180,6 +241,12 @@ class Personaje:
         self.ask.grab_set()
         self.ask.wait_window(self.ask)
     
+    def updatelabel(self, points):
+        temp = self.puntos
+        for item in self.values:
+            temp-=item.get()
+        self.pointsleft.set("Puntos restantes: "+str(temp))
+
     def check(self):
         for i in range(5):
             self.atributos[i+1] = self.values[i].get()
@@ -200,24 +267,31 @@ class Personaje:
                 self.master.focus_set()
     
     def taketest(self, test):
+        messagebox.showinfo("Resumen Interrogación","Gastaste "+str(self.tiempo2-self.tiempo))
         self.delta=0
         temp = 0
-        temp += floor(self.bon[0]*self.atributos[2])+self.temp[0]-test.destreza
-        temp += floor(self.bon[1]*self.atributos[3])+self.temp[1]-test.resistencia
-        temp += floor(self.bon[2]*self.atributos[4])+self.temp[2]-test.suerte
-        temp += floor(self.bon[3]*self.atributos[5])+self.temp[3]-test.inteligencia
-        if temp<0:
-            self.delta = temp*(test.vida//self.atributos[self.stats[test.debilidad]])
+        temp += math.floor(self.bon[0]*self.atributos[2])+self.temp[0]-int(test.destreza)
+        temp += math.floor(self.bon[1]*self.atributos[3])+self.temp[1]-int(test.resistencia)
+        temp += math.floor(self.bon[2]*self.atributos[4])+self.temp[2]-int(test.suerte)
+        temp += math.floor(self.bon[3]*self.atributos[5])+self.temp[3]-int(test.inteligencia)
+        
+        if temp < 0:
+            if self.atributos[int(self.stats[test.debilidad])+1]==0:
+                self.delta = temp*int(test.vida)
+            else:
+                self.delta = temp*(int(test.vida)//self.atributos[int(self.stats[test.debilidad])])
             self.atributos[1]+=self.delta
         self.temp = 4*[0]
         self.consumables = []
+        self.tiempo2 = self.tiempo
             
     def consume(self, consumable):
+        self.tiempo-=consumable.costo
         self.temp[self.stats[consumable.atributo]-1]+=consumable.tempbon
         self.consumables.append(consumable)
 
     def equip(self, equipment):
-        self.bon[self.stats[equipment.atributo]-1]+=equipment.bon
+        self.bon[self.stats[str(equipment.atributo)]-1]+=equipment.bon
         self.equipment.append(equipment)
 
 class Prueba:
@@ -230,12 +304,18 @@ class Prueba:
         self.suerte = parameter_list[5]
         self.debilidad = parameter_list[6]
 
+    def __str__(self):
+        return str(self.nombre)+": V:"+str(self.vida)+" D:"+str(self.destreza)+" R:"+str(self.resistencia)+" I:"+str(self.inteligencia)+" S:"+str(self.suerte)+" Debilidad:"+str(self.debilidad)
+
 class Consumible:
     def __init__(self, parameter_list):
         self.nombre = parameter_list[0]
         self.atributo = parameter_list[1]
         self.tempbon = int(parameter_list[2])
         self.costo = int(parameter_list[3])
+
+    def __str__(self):
+        return str(self.nombre)+": Costo de tiempo "+str(self.costo)+" Bonus "+str(self.tempbon)+" a "+self.atributo
 
 class Equipamiento:
     def __init__(self, parameter_list):
